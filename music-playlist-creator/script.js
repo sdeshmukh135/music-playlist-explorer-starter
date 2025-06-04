561daee57e40d632075d6cc36ebebc26596dd2c4
Sara Deshmukh <saradeshmukh@meta.com>
2025-06-03 23:49:04 -0700

    static display no modal

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,21 @@
+// JavaScript for Opening and Closing the Modal
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('PlaylistName').innerText = playlist.name;
+   document.getElementById('PlaylistImage').src = playlist.imageUrl;
+   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
+   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
+   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
