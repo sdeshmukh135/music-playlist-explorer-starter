c2681f01ea366fe2a96d922cce351ecd1d44672a
Sara Deshmukh <saradeshmukh@meta.com>
2025-06-05 00:42:36 -0700

    dynamic playlists, fix dynamic modal

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,210 @@
+let playlists = [
+    {
+		"playlistId": 1, // first playlist object
+		"playlist_name": "Fun Stuff",
+		"playlist_author": "CoolPerson200",
+		"playlist_art": "assets/img/album1.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "The Travelled Road",
+                "song_artist":"Evan Mack",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "The Travelled Road",
+                "song_artist":"Evan Mack",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "The Travelled Road",
+                "song_artist":"Evan Mack",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+	},
+	{
+        "playlistId": 2, // first playlist object
+		"playlist_name": "Even More Fun Stuff",
+		"playlist_author": "CoolPerson300",
+		"playlist_art": "assets/img/album2.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Race",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Race",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Race",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 3, // first playlist object
+		"playlist_name": "This is Fun",
+		"playlist_author": "CoolPerson400",
+		"playlist_art": "assets/img/album3.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Unaired",
+                "song_artist":"Hozier",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Unaired",
+                "song_artist":"Hozier",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Unaired",
+                "song_artist":"Hozier",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 4, // first playlist object
+		"playlist_name": "More Fun",
+		"playlist_author": "CoolPerson340",
+		"playlist_art": "assets/img/album4.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Mind of Mine",
+                "song_artist":"Zayn",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Mine of Mine",
+                "song_artist":"Zayn",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Mind of Mine",
+                "song_artist":"Zayn",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 5, // first playlist object
+		"playlist_name": "Fun Stuff More",
+		"playlist_author": "CoolPerson405",
+		"playlist_art": "assets/img/album5.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Don't Remember",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Don't Remember",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Don't Remeber",
+                "song_artist":"Unknown",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 6, // first playlist object
+		"playlist_name": "Fun Stuff Yep",
+		"playlist_author": "CoolPerson204",
+		"playlist_art": "assets/img/album6.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Untitled",
+                "song_artist":"SZA",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Untitled",
+                "song_artist":"SZA",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Untitled",
+                "song_artist":"SZA",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 7, // first playlist object
+		"playlist_name": "Fun Stuff Definitely",
+		"playlist_author": "CoolPerson248",
+		"playlist_art": "assets/img/album7.png",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "IDK",
+                "song_artist":"Mac DeMarco",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "IDK",
+                "song_artist":"Mac DeMarco",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "IDK",
+                "song_artist":"Mac DeMarco",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    },
+    {
+        "playlistId": 8, // first playlist object
+		"playlist_name": "Fun Stuff Finally",
+		"playlist_author": "CoolPerson234",
+		"playlist_art": "assets/img/album8.jpeg",
+		"songs": [ // object inside of an object
+            {
+                "song_title": "Eight Miles High",
+                "song_artist":"The Golding Earning",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Eight Miles High",
+                "song_artist":"The Golding Earning",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            },
+            {
+                "song_title": "Eight Miles High",
+                "song_artist":"The Golding Earning",
+                "duration": "20:00",
+                "song_art": "assets/img/song.png"
+            }
+        ]
+    }
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,14 +1,79 @@
-// JavaScript for Opening and Closing the Modal
+// JavaScript for Opening and Closing the Modal (new screen)
 const modal = document.getElementById("playlistModal");
-const span = document.getElementsByClassName("close")[0];
+const span = document.querySelectorAll("song"); // array of songs
 
-function openModal(playlist) {
-   document.getElementById('PlaylistName').innerText = playlist.name;
-   document.getElementById('PlaylistImage').src = playlist.imageUrl;
-   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
-   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
-   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+//console.log(span);
+
+
+const openModal = (playlist) => {
+    console.log("_____");
+   document.getElementById('PlaylistName').innerText = playlist.playlist_name;
+   document.getElementById('PlaylistImage').src = playlist.playlist_art;
+   document.getElementById('CreatorName').innerText = playlist.playlist_author;
+    console.log("______");
+    // for the song element
+   document.addEventListener("DOMContentLoaded", loadSongs());
    modal.style.display = "block";
+}
+
+const loadPlaylists = () => {
+    console.log("playlists loading")
+    const container = document.querySelector(".playlist-cards");
+    if (playlists === null) { // check if null or undefined
+        console.log("No playlists added");
+    } else {
+        for (const playlist of playlists) {
+            const el = createPlaylistElement(playlist);
+            container.append(el);
+        }
+    }
+
+}
+
+const createPlaylistElement = (playlist) => {
+    console.log(playlist);
+    const playElement = document.createElement('section');
+    playElement.className = "playlist-card";
+    playElement.innerHTML = `
+    <img src=${playlist.playlist_art}/>
+    <h3>${playlist.playlist_name}</h3>
+    <h4>${playlist.playlist_author}</h4>
+    <div class="likes">
+        <img src="assets/img/heart-icon.webp"/>
+        <h4>0</h4>
+    </div>
+    <div>
+        <button onclick="openModal(${playlist})"></button>
+    </div>`;
+    return playElement;
+}
+
+document.addEventListener("DOMContentLoaded", loadPlaylists());
+
+const loadSongs = (playlist) => {
+    console.log("_____");
+    const container = document.querySelector(".songs");
+    if (playlist.songs === null) {
+        console.log("There are no songs present");
+    } else {
+        for (const song of playlist.songs) {
+            const el = createSongElement(song);
+            container.append(el);
+        }
+    }
+}
+
+const createSongElement = (song) => {
+    console.log(song);
+    const songElement = document.createElement('section');
+    songElement.className = 'song';
+    songElement.innerHTML = `
+    <img id="SongImage" src=${song.song_art} alt="Song Image">
+    <h4 class="songTitle">${song.song_title}</h4>
+    <h4 class="artistName">${song.song_artist}</h4>
+    <h4 class="times">${song.duration}</h4>
+    `;
+    return songElement;
 }
 
 span.onclick = function() {
